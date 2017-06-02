75bd463f0007767360ebeb38072c139fb01a743c
Yamashou <1230124fw@gmail.com>
2017-06-02 22:00:41 +0900

    おみくじ

--- app/src/main/java/com/techcetro/firastapp/MainActivity.kt
+++ app/src/main/java/com/techcetro/firastapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.graphics.Color
+import java.util.Random
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun changeText(view: View) {
-        val textView = findViewById(R.id.text_view) as TextView
-        textView.text = "Changed from Kotlin"
+        val results = listOf("大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶")
+        val random = Random().nextInt(results.count())
+        if(random == 0) {
+            resultText.setTextColor(Color.RED)
+        } else {
+            resultText.setTextColor(Color.BLACK)
+        }
+        resultText.text = results[random]
     }
 }
